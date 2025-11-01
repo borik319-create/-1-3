6b61c9b86cae139e0c9417fb649f07587f525af7
borik319-create <borik319@gmail.com>
2025-11-01 18:14:58 +0300

    Добавьте файлы проекта.

---
+++ ЛР1 №3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ЛР1__3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите пол: ");
+            string g = Console.ReadLine().ToLower();
+
+            if (g == "м")
+            {
+                Console.WriteLine("Возможные мужские имена: ");
+                Console.WriteLine("Алексей");
+                Console.WriteLine("Владимир");
+                Console.WriteLine("Кирилл");
+            }
+            else if(g == "ж")
+            {
+                Console.WriteLine("Возможные женские имена: ");
+                Console.WriteLine("Анастасия");
+                Console.WriteLine("Екатирина");
+                Console.WriteLine("Татьяна");
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод. Введите 'м' или 'ж' ");
+                
+            }
+        }
+    }
+}
